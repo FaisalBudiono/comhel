8643c35d78757c4d650b507f6f592c2969d88c34
Faisal Budiono <ucul.project@gmail.com>
2025-03-17 16:01:56 +0700

    feat(log): add prod mode logging in home dir

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # ComHel
 Docker `Com`pose `Hel`per is just a command helper to help run docker compose up and down
+
+# Logs
+You can see the logs on `[home-dir]/.comhel/logs.log`
--- internal/app/adapter/log/logger.go
+++ internal/app/adapter/log/logger.go
@@ -3,6 +3,8 @@ package log
 import (
 	"log/slog"
 	"os"
+	"os/user"
+	"path/filepath"
 	"strings"
 )
 
@@ -17,7 +19,7 @@ func SetDefault(l *slog.Logger) {
 }
 
 func NewLogger() (*slog.Logger, error) {
-	f, err := os.OpenFile("./logs/comhel.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	f, err := os.OpenFile(logPath(), os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
 		return nil, err
 	}
@@ -27,6 +29,30 @@ func NewLogger() (*slog.Logger, error) {
 	})
 
 	return slog.New(slogHandler), nil
+}
+
+func logPath() string {
+	devMode := strings.ToLower(os.Getenv("DEV_MODE")) == "true"
+	if devMode {
+		return "./logs/logs.log"
+	}
+
+	return prodLogPath()
+}
+
+func prodLogPath() string {
+	u, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+
+	dir := filepath.Join(u.HomeDir, ".comhel")
+	err = os.MkdirAll(dir, 0700)
+	if err != nil {
+		panic(err)
+	}
+
+	return filepath.Join(dir, "logs.log")
 }
 
 func logLevel() slog.Leveler {
